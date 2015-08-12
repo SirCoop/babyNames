6122244c150a06058a2598b00fec1df924e3e0e2
SirCoop <cooper.garym@gmail.com>
2015-08-11 22:18:30 -0400

    Removed some comments

--- constants.js
+++ constants.js
@@ -7,30 +7,6 @@ var path = require('path');
 module.exports = function(opts) {
     return {
 
-        //DB_URI: 'mongodb://localhost/ebgi_gallery',
-
-        //UPLOADBASE: __dirname + '/backend/upload/',
-
-        //PORT: {
-        //    DATASERVICE: 9000,
-        //    ANEB: 9999,
-        //    CMS: 10001,
-        //    GALLERY: 9888,
-        //    FILESERVICE: 12121,
-        //    AUTHSERVICE: 9777,
-        //    GATEWAY: 80
-        //},
-
-        //MODULE_REQUIRE: {
-        //    BACKEND: __dirname + '/backend',
-        //
-        //    TEST: {
-        //        BACKEND: __dirname + '/test/backend',
-        //        ANEB: __dirname + '/test/aneb',
-        //        CMS: __dirname + 'test/cms'
-        //    }
-        //},
-
         APPPARENT: __dirname + '/../',
         APPBASE: __dirname,
         PAGESJSON: path.join(__dirname, '/app/_resources/BabyNames.json'),
@@ -46,12 +22,6 @@ module.exports = function(opts) {
             api: 1
         },
         DB_URI: 'mongodb://localhost/babyNames',
-        //DB_SERVICE: path.join(__dirname, '/app/dbService')
-
-        //DATASERVICE: {
-        //    DSBASE: __dirname + '/backend/service/DataService/_dsBase',
-        //    DSARTIST: __dirname + '/backend/service/DataService/dsArtist',
-        //},
 
         MODEL: {
             BABYNAME: path.join(__dirname, '/dbService/model/BabyName')
